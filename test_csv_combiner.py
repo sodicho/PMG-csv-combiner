fd0ad97b24eed9f5dabd1e2c14623400a3c7be4c
sodicho <35441499+sodicho@users.noreply.github.com>
2021-04-12 22:35:08 -0500

    Add files via upload
    Unit tests

---
+++ test_csv_combiner.py
@@ -0,0 +1,25 @@
+import unittest
+from sodicho_csv_combiner import get_name
+
+class Combiner_Tests(unittest.TestCase):
+    
+    def setUp(self):
+        print("setUp called..")
+        self.csv = 'random.csv'
+        self.path = '~/Random/things/clear/file/correct.csv'
+
+    def tearDown(self):
+        self.csv = ''
+        print("tearDown called..")
+
+    def test_csv(self):
+        print('Testing file for CSV')
+        self.assertEqual(self.csv[-4:],'.csv', "Not a csv file")
+    
+    def test_name(self):
+        print('Testing name getter')
+        self.assertEqual(get_name(self.path), 'correct.csv', "Wrong file name")
+        
+
+if __name__ == "__main__":
+    unittest.main()
